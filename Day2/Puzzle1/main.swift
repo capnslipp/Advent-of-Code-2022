edb9b0f671cafb9fcdf06c5ac6741343efd46514
Slipp Douglas Thompson <git@capnslipp.com>
2022-12-24 06:18:47 -0600

    Initial input file reading setup
    * Initial input file reading setup, based on what was done on Day1.
    * Added a StringExtensions file with a `split(separator: CharacterSet)` alias of `components(separatedBy:)` because the naming of the latter was annoying me.

--- Day2/Puzzle1/main.swift
+++ Day2/Puzzle1/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+let inputFilename = "input.example"
+//let inputFilename = "input"
+let inputURL = Bundle.main.url(forResource: inputFilename, withExtension: "")!
+let inputContents = try! String(contentsOf: inputURL)
+let inputLines = inputContents.components(separatedBy: .newlines)
+
+for inputLine in inputLines {
+	guard !inputLine.isEmpty else { continue }
+	
+	let inputPieces = inputLine.split(separator: CharacterSet.whitespaces)
+	guard inputPieces.count >= 2 else {
+		fatalError("Line “\(inputLine)” does not contain at least 2 fields (whitespace-separated).")
+	}
+	print("\(inputPieces[0]) -> \(inputPieces[1])")
+}
---
+++ Support/Extensions/StringExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  StringExtensions.swift
+//  
+//
+//  Created by Cap'n Slipp on 12/24/22.
+//
+
+import Foundation
+
+
+
+extension StringProtocol
+{
+	/// Alias of `components(separatedBy:)`, so it's easy to find this when code completing for `split…`.
+	func split(separator: CharacterSet) -> [String] {
+		return self.components(separatedBy: separator)
+	}
+}
